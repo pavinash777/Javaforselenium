dbf32dccd6994681764c258b142df3934b141989
Avinash <avinashpottipadu7@gmnail.com>
2024-01-18 17:33:30 +0530

    18 Jan 2024

---
+++ src/main/java/javabasicprograms/PrintNumbersAndText.java
@@ -0,0 +1,34 @@
+package javabasicprograms;
+
+public class PrintNumbersAndText {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Print method
+		System.out.print("Code Collaborate");
+		System.out.print("Learn Java");
+		
+		//Print Text using Println
+		System.out.println("One");
+		System.out.println("Two");
+		System.out.println("Three");
+		System.out.println("Four");
+		System.out.println("Five");
+		System.out.println("Six");
+		System.out.println("Seven");
+		
+		//Print numbers
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
+		
+		
+
+	}
+
+}
